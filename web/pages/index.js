4398bb1ce82ff7ad652de49eddf4f86d06d5af81
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-04-05 20:33:08 +0530

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useDispatch } from 'react-redux';
+import { fetchPosts } from '../redux/actions/postActions';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
